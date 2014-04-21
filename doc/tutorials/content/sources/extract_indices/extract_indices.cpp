3b7db86254fdb1167577935e6fcdea5fed097e47
Niklas Hambüchen <mail@nh2.me>
2014-04-21 23:16:35 +0100

    extract_indices: Pull out all headers to make precompiled headers easy

--- doc/tutorials/content/sources/extract_indices/extract_indices.cpp
+++ doc/tutorials/content/sources/extract_indices/extract_indices.cpp
@@ -1,14 +1,4 @@
-#include <iostream>
-#include <stdexcept>
-#include <pcl/common/intersections.h>
-#include <pcl/ModelCoefficients.h>
-#include <pcl/io/pcd_io.h>
-#include <pcl/point_types.h>
-#include <pcl/sample_consensus/method_types.h>
-#include <pcl/sample_consensus/model_types.h>
-#include <pcl/segmentation/sac_segmentation.h>
-#include <pcl/filters/voxel_grid.h>
-#include <pcl/filters/extract_indices.h>
+#include "extract_indices.h"
 
 int
 main (int argc, char** argv)
---
+++ doc/tutorials/content/sources/extract_indices/extract_indices.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdexcept>
+#include <pcl/common/intersections.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/extract_indices.h>
