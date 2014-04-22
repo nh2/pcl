9aa4b4ae5260b1a79465c969f542e916e9624185
Niklas Hambüchen <mail@nh2.me>
2014-04-22 22:22:08 +0100

    Enough std:: and pcl:: - kill them

--- doc/tutorials/content/sources/extract_indices/extract_indices.cpp
+++ doc/tutorials/content/sources/extract_indices/extract_indices.cpp
@@ -1,5 +1,8 @@
 #include "extract_indices.h"
 
+using namespace std;
+using namespace pcl;
+
 const int num_colors = 4;
 int plane_colors[num_colors][3] = {
   {   0, 255,   0 },
@@ -11,46 +14,46 @@ int plane_colors[num_colors][3] = {
 int
 main (int argc, char** argv)
 {
-  pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr cloud_blob (new pcl::PointCloud<pcl::PointXYZRGBNormal>),
-                                               cloud_filtered_blob (new pcl::PointCloud<pcl::PointXYZRGBNormal>);
-  pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZRGBNormal>),
-                                               cloud_p (new pcl::PointCloud<pcl::PointXYZRGBNormal>),
-                                               cloud_f (new pcl::PointCloud<pcl::PointXYZRGBNormal>);
+  PointCloud<PointXYZRGBNormal>::Ptr cloud_blob (new PointCloud<PointXYZRGBNormal>),
+                                     cloud_filtered_blob (new PointCloud<PointXYZRGBNormal>);
+  PointCloud<PointXYZRGBNormal>::Ptr cloud_filtered (new PointCloud<PointXYZRGBNormal>),
+                                     cloud_p (new PointCloud<PointXYZRGBNormal>),
+                                     cloud_f (new PointCloud<PointXYZRGBNormal>);
 
   if (argc != 7)
   {
-    throw std::runtime_error("\n"
-                             "Usage: extract_indices [file.pcd] [rest_ratio] [max_iterations] [distance_threshold] [optimize_coefficients] [downsample_size]\n"
-                             "defaults:                             0.3            1000               0.01                   true                 0.01");
+    throw runtime_error("\n"
+                        "Usage: extract_indices [file.pcd] [rest_ratio] [max_iterations] [distance_threshold] [optimize_coefficients] [downsample_size]\n"
+                        "defaults:                             0.3            1000               0.01                   true                 0.01");
   }
 
-  std::string filename = argv[1];
+  string filename = argv[1];
   double rest_ratio = atof(argv[2]);          // default: 0.3
   int max_iterations = atoi(argv[3]);         // default: 1000
   double distance_threshold = atof(argv[4]);  // default: 0.01
-  std::string opt_coeff_str = argv[5];
+  string opt_coeff_str = argv[5];
   bool optimize_coefficients =                // default: true
        opt_coeff_str == "true"  ? true
-    : (opt_coeff_str == "false" ? false : throw std::runtime_error("bad optimize_coefficients value"));
+    : (opt_coeff_str == "false" ? false : throw runtime_error("bad optimize_coefficients value"));
   double downsample_size = atof(argv[6]);     // default: 0.01
   bool downsample = downsample_size != atof("0");
 
   // Fill in the cloud data
-  pcl::io::loadPCDFile (filename, *cloud_blob);
+  io::loadPCDFile (filename, *cloud_blob);
 
-  std::cerr << "PointCloud: " << cloud_blob->width * cloud_blob->height << " data points." << std::endl;
+  cerr << "PointCloud: " << cloud_blob->width * cloud_blob->height << " data points." << endl;
 
   if (downsample)
   {
     // Create the filtering object: downsample the dataset using a leaf size of 1cm
-    pcl::VoxelGrid<pcl::PointXYZRGBNormal> sor;
+    VoxelGrid<PointXYZRGBNormal> sor;
     sor.setInputCloud (cloud_blob);
     sor.setLeafSize (downsample_size, downsample_size, downsample_size);
     sor.filter (*cloud_filtered_blob);
 
     *cloud_filtered = *cloud_filtered_blob;
 
-    std::cerr << "PointCloud after filtering: " << cloud_filtered->width * cloud_filtered->height << " data points." << std::endl;
+    cerr << "PointCloud after filtering: " << cloud_filtered->width * cloud_filtered->height << " data points." << endl;
   }
   else
   {
@@ -58,27 +61,27 @@ main (int argc, char** argv)
   }
 
   // Write the downsampled version to disk
-  pcl::io::savePCDFile ("cloud_downsampled.pcd", *cloud_filtered, true);
+  io::savePCDFile ("cloud_downsampled.pcd", *cloud_filtered, true);
 
-  pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients ());
-  pcl::PointIndices::Ptr inliers (new pcl::PointIndices ());
+  ModelCoefficients::Ptr coefficients (new ModelCoefficients ());
+  PointIndices::Ptr inliers (new PointIndices ());
   // Create the segmentation object
-  // pcl::SACSegmentation<pcl::PointXYZ> seg;
-  pcl::SACSegmentationFromNormals<pcl::PointXYZRGB, pcl::Normal> seg; // for normals
+  // SACSegmentation<PointXYZ> seg;
+  SACSegmentationFromNormals<PointXYZRGB, Normal> seg; // for normals
   // Optional
   seg.setOptimizeCoefficients (optimize_coefficients);
   // Mandatory
-  // seg.setModelType (pcl::SACMODEL_PLANE);
-  seg.setModelType (pcl::SACMODEL_NORMAL_PLANE); // for normals
+  // seg.setModelType (SACMODEL_PLANE);
+  seg.setModelType (SACMODEL_NORMAL_PLANE); // for normals
   seg.setNormalDistanceWeight (0.1); // for normals
 
-  seg.setMethodType (pcl::SAC_RANSAC);
+  seg.setMethodType (SAC_RANSAC);
   seg.setMaxIterations (max_iterations);
   seg.setDistanceThreshold (distance_threshold);
 
   // Copy XYZRGBNNormal cloud to two clouds (XYZRGB + Normal) because SACSegmentationFromNormals wants them separate
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered_xyz (new pcl::PointCloud<pcl::PointXYZRGB>);
-  pcl::PointCloud<pcl::Normal>::Ptr      cloud_filtered_normals (new pcl::PointCloud<pcl::Normal>);
+  PointCloud<PointXYZRGB>::Ptr cloud_filtered_xyz (new PointCloud<PointXYZRGB>);
+  PointCloud<Normal>::Ptr      cloud_filtered_normals (new PointCloud<Normal>);
   cloud_filtered_xyz->resize(cloud_filtered->size());
   cloud_filtered_normals->resize(cloud_filtered->size());
   for (int i = 0; i < cloud_filtered->size(); ++i)
@@ -88,9 +91,9 @@ main (int argc, char** argv)
   }
 
   // Create the filtering object
-  pcl::ExtractIndices<pcl::PointXYZRGBNormal> extract;
+  ExtractIndices<PointXYZRGBNormal> extract;
 
-  std::vector<Eigen::Vector4f> planes;
+  vector<Eigen::Vector4f> planes;
 
   int i = 0, nr_points = (int) cloud_filtered->points.size ();
   // While (rest_ratio * 100%) of the original cloud is still there
@@ -103,11 +106,11 @@ main (int argc, char** argv)
     seg.segment (*inliers, *coefficients);
     if (inliers->indices.size () == 0)
     {
-      std::cerr << "Could not estimate a planar model for the given dataset." << std::endl;
+      cerr << "Could not estimate a planar model for the given dataset." << endl;
       break;
     }
 
-    std::cout << "Cloud " << i << " coefficients: " << *coefficients << std::endl;
+    cout << "Cloud " << i << " coefficients: " << *coefficients << endl;
 
     planes.push_back(Eigen::Vector4f::Map(&coefficients->values[0]));
 
@@ -116,14 +119,14 @@ main (int argc, char** argv)
     extract.setIndices (inliers);
     extract.setNegative (false);
     extract.filter (*cloud_p);
-    std::cerr << "PointCloud representing the planar component: " << cloud_p->width * cloud_p->height << " data points." << std::endl;
+    cerr << "PointCloud representing the planar component: " << cloud_p->width * cloud_p->height << " data points." << endl;
 
     // Set plane color
-    pcl::PointCloud<pcl::PointXYZRGB> cloud_colored;
+    PointCloud<PointXYZRGB> cloud_colored;
     for (int pi = 0; pi < cloud_p->size(); ++pi)
     {
-      pcl::PointXYZRGBNormal p = (*cloud_p)[pi];
-      pcl::PointXYZRGB cp (plane_colors[i % num_colors][0],
+      PointXYZRGBNormal p = (*cloud_p)[pi];
+      PointXYZRGB cp (plane_colors[i % num_colors][0],
                            plane_colors[i % num_colors][1],
                            plane_colors[i % num_colors][2]);
       cp.x = p.x;
@@ -132,23 +135,23 @@ main (int argc, char** argv)
       cloud_colored.push_back(cp);
     }
 
-    std::stringstream ss;
+    stringstream ss;
     ss << "cloud_plane_" << i << ".pcd";
-    // pcl::io::savePCDFile (ss.str (), *cloud_p, true);
-    pcl::io::savePCDFile (ss.str (), cloud_colored, true);
+    // io::savePCDFile (ss.str (), *cloud_p, true);
+    io::savePCDFile (ss.str (), cloud_colored, true);
 
     // Create the filtering object
     extract.setNegative (true);
     extract.filter (*cloud_f);
     cloud_filtered.swap (cloud_f);
 
-    pcl::ExtractIndices<pcl::PointXYZRGB> extract_xyz;
+    ExtractIndices<PointXYZRGB> extract_xyz;
     extract_xyz.setInputCloud (cloud_filtered_xyz);
     extract_xyz.setIndices (inliers);
     extract_xyz.setNegative (true);
     extract_xyz.filter (*cloud_filtered_xyz);
 
-    pcl::ExtractIndices<pcl::Normal> extract_normals;
+    ExtractIndices<Normal> extract_normals;
     extract_normals.setInputCloud (cloud_filtered_normals);
     extract_normals.setIndices (inliers);
     extract_normals.setNegative (true);
@@ -158,58 +161,58 @@ main (int argc, char** argv)
     i++;
   }
 
-  std::ofstream planes_file("planes.txt");
+  ofstream planes_file("planes.txt");
   for (int i = 0; i < planes.size(); ++i)
   {
     Eigen::Vector4f p = planes[i];
-    planes_file << p[0] << " " << p[1] << " " << p[2] << " " << p[3] << std::endl;
+    planes_file << p[0] << " " << p[1] << " " << p[2] << " " << p[3] << endl;
   }
 
 
   if (planes.size() < 3)
   {
-    std::cout << "Not enough planes" << std::endl;
+    cout << "Not enough planes" << endl;
   }
   else
   {
-    std::cout << "Cutting planes" << std::endl;
+    cout << "Cutting planes" << endl;
 
     Eigen::VectorXf cut_line1;
-    bool ok1 = pcl::planeWithPlaneIntersection (planes[0], planes[1], cut_line1, 0.1);
+    bool ok1 = planeWithPlaneIntersection (planes[0], planes[1], cut_line1, 0.1);
 
     Eigen::VectorXf cut_line2;
-    bool ok2 = pcl::planeWithPlaneIntersection (planes[0], planes[2], cut_line2, 0.1);
+    bool ok2 = planeWithPlaneIntersection (planes[0], planes[2], cut_line2, 0.1);
 
     if (!ok1 || !ok2)
     {
-      std::cout << "cut failed" << std::endl;
+      cout << "cut failed" << endl;
     }
     else
     {
-      std::cout << std::endl << "cut line 1:" << std::endl << cut_line1 << std::endl;
-      std::cout << std::endl << "cut line 2:" << std::endl << cut_line2 << std::endl;
+      cout << endl << "cut line 1:" << endl << cut_line1 << endl;
+      cout << endl << "cut line 2:" << endl << cut_line2 << endl;
 
       Eigen::Vector4f corner;
-      bool ok3 = pcl::lineWithLineIntersection (cut_line1, cut_line2, corner, 1e-4);
+      bool ok3 = lineWithLineIntersection (cut_line1, cut_line2, corner, 1e-4);
 
       if (!ok3)
       {
-        std::cout << std::endl << "finding corner failed" << std::endl;
+        cout << endl << "finding corner failed" << endl;
       }
       else
       {
-        std::cout << std::endl << "corner point:" << std::endl << corner << std::endl;
+        cout << endl << "corner point:" << endl << corner << endl;
 
-        pcl::PointXYZ corner_point (corner[0], corner[1], corner[2]);
+        PointXYZ corner_point (corner[0], corner[1], corner[2]);
 
-        pcl::PointXYZRGB corner_point_color(255,0,0);
+        PointXYZRGB corner_point_color(255,0,0);
         corner_point_color.x = corner_point.x;
         corner_point_color.y = corner_point.y;
         corner_point_color.z = corner_point.z;
 
-        pcl::PointCloud<pcl::PointXYZRGB> corner_cloud;
+        PointCloud<PointXYZRGB> corner_cloud;
         corner_cloud.push_back (corner_point_color);
-        pcl::io::savePCDFile ("cloud_corners.pcd", corner_cloud, false);
+        io::savePCDFile ("cloud_corners.pcd", corner_cloud, false);
       }
     }
   }
