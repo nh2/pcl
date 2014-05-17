7649660dfd214ee1e9179e1a848e7b81b2b1d4d0
Niklas Hambüchen <mail@nh2.me>
2014-05-17 23:31:32 +0100

    pcl_viewer: Move forward/backward with KeyUp/KeyDown

--- visualization/src/pcl_visualizer.cpp
+++ visualization/src/pcl_visualizer.cpp
@@ -1811,6 +1811,8 @@ pcl::visualization::PCLVisualizer::setCameraPosition (
       cam->SetPosition (pos_x, pos_y, pos_z);
       cam->SetFocalPoint (view_x, view_y, view_z);
       cam->SetViewUp (up_x, up_y, up_z);
+
+      cam->SetClippingRange (0.01, 1000.01);
     }
     ++i;
   }
