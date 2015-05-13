7d6580af4cb01d3f397818bc0b41b4c26eb92d10
Niklas Hambüchen <mail@nh2.me>
2015-05-13 19:33:56 +0000

    Initial TCPGrabber

---
+++ io/src/tcp_grabber.cpp
@@ -0,0 +1,199 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Point Cloud Library (PCL) - www.pointclouds.org
+ *  Copyright (c) 2012-, Open Perception, Inc.
+ *
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the copyright holder(s) nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#include <pcl/pcl_config.h>
+
+#include <pcl/io/tcp_grabber.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/common/time.h>
+#include <pcl/common/io.h>
+#include <pcl/console/print.h>
+#include <pcl/io/boost.h>
+#include <pcl/exceptions.h>
+
+#include <boost/asio.hpp>
+#include <boost/thread.hpp>
+
+#include <iostream>
+#include <queue>
+
+using boost::asio::ip::tcp;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+pcl::TCPGrabber::TCPGrabber ()
+  : running_ (false)
+  , fps_ (0.0f)
+{
+  depth_image_signal_ = createSignal<sig_cb_tcp_depth_image> ();
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+pcl::TCPGrabber::~TCPGrabber () throw ()
+{
+  stop ();
+
+  disconnect_all_slots<sig_cb_tcp_depth_image> ();
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+bool
+pcl::TCPGrabber::init ()
+{
+
+  return (true);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void
+pcl::TCPGrabber::close ()
+{
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void
+pcl::TCPGrabber::start ()
+{
+  init ();
+  running_ = true;
+
+  grabber_thread_ = boost::thread (&pcl::TCPGrabber::processGrabbing, this);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void
+pcl::TCPGrabber::stop ()
+{
+  running_ = false;
+  grabber_thread_.join ();
+
+  close ();
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+bool
+pcl::TCPGrabber::isRunning () const
+{
+  return (running_);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+std::string
+pcl::TCPGrabber::getName () const
+{
+  return (std::string ("TCPGrabber"));
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+float
+pcl::TCPGrabber::getFramesPerSecond () const
+{
+  fps_mutex_.lock ();
+  float fps = fps_;
+  fps_mutex_.unlock ();
+
+  return (fps);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void
+pcl::TCPGrabber::processGrabbing ()
+{
+
+  pcl::StopWatch stop_watch;
+  std::queue<double> capture_time_queue;
+  double total_time = 0.0f;
+
+  stop_watch.reset ();
+
+  using namespace boost;
+
+  asio::io_service io_service;
+
+  tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 13));
+
+  // socket accept loop
+  for (;;)
+  {
+    tcp::socket socket(io_service);
+    acceptor.accept(socket);
+
+    boost::array<char, 640*480> buf;
+
+    boost::system::error_code error_code;
+    // asio::write(socket, asio::buffer(message), asio::transfer_all(), ignored_error);
+    asio::read(socket, asio::buffer(buf), boost::asio::transfer_at_least(buf.size()), error_code);
+
+    if (error_code)
+    {
+      PCL_THROW_EXCEPTION (pcl::IOException, "TCPGrabber: Could not read from socket");
+    }
+
+    bool continue_grabbing = true;
+    while (continue_grabbing)
+    {
+      // Acquire frame
+
+      uint8_t *frame_data;
+      const boost::shared_ptr<unsigned char> frame_data_ptr(frame_data);
+
+      // publish frame
+      if (num_slots<sig_cb_tcp_depth_image> () > 0)
+      {
+        depth_image_signal_->operator() (frame_data_ptr);
+      }
+
+      const double capture_time = stop_watch.getTimeSeconds ();
+      total_time += capture_time;
+
+      capture_time_queue.push (capture_time);
+
+      if (capture_time_queue.size () >= 30)
+      {
+        double removed_time = capture_time_queue.front ();
+        capture_time_queue.pop ();
+
+        total_time -= removed_time;
+      }
+
+      fps_mutex_.lock ();
+      fps_ = static_cast<float> (total_time / capture_time_queue.size ());
+      fps_mutex_.unlock ();
+    }
+
+  }
+}
