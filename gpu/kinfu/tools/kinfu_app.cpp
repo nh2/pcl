45316ff9cca6a448dae23e9dee261149a3a807e6
Niklas Hambüchen <mail@nh2.me>
2015-05-24 15:41:37 +0200

    Remove ASCII ply fixing function.
    Apparently the Python file automatically fixed it.

--- gpu/kinfu/tools/kinfu_app.cpp
+++ gpu/kinfu/tools/kinfu_app.cpp
@@ -903,7 +903,6 @@ struct KinFuApp
 	assert(res == 0);
     res = system("python ply2asc\\ply2asc.py mesh-clean.ply mesh-rotated.asc.ply");
 	assert(res == 0);
-	fixPlyFile("mesh-rotated.asc.ply", "mesh-rotated-fixed.asc.ply");
 
     cout << "Cropping face" << endl;
     pcl::PolygonMesh mesh;
@@ -1878,74 +1877,3 @@ const int COL_NUM = 10;
 //   9 property uchar alpha   ->  5 property float nz
 int col_order[COL_NUM] = { 0, 1, 2, 6, 7, 8, 9, 3, 4, 5 };
 
-int fixPlyFile(const char *meshIn, const char *meshOut)
-{
-	ifstream inputFile;
-	ofstream outputFile;
-	string line;
-	inputFile.open(meshIn);
-	outputFile.open(meshOut);
-	vector<string> found_properties;
-	bool fixing_properties = false;
-	// Iterate over the lines of the .ply file
-	while (!inputFile.eof()) {
-		getline(inputFile, line);
-
-		// First find "element vertex" line, then start fixing properties.
-		if (line.find("element vertex") == 0) {
-			outputFile << line << endl;
-			fixing_properties = true;
-		}
-		// Save any "property ..." line that's after "element vertex", we want to reorder them.
-		else if (line.find("property") == 0 && fixing_properties) {
-			found_properties.push_back(line);
-		}
-		// When we're at the next element (usually "element face"), we're past all properties.
-		else if (line.find("element") == 0) {
-			// Now print out all properties in the order we want.
-			assert(found_properties.size() == COL_NUM);
-			for (int i = 0; i < COL_NUM; i++) {
-				outputFile << found_properties[col_order[i]] << endl;
-			}
-			outputFile << line << endl;
-			fixing_properties = false;
-		}
-		// Once we're at end_header, vertices will start.
-		// We want to reorder them to the same order as we did with the headers.
-		else if (line.find("end_header") == 0) {
-			outputFile << line << endl;
-
-			// Go over all vertex lines, reordering their columns.
-			while (!inputFile.eof()) {
-				getline(inputFile, line);
-				int start = 0;
-				istringstream linestream(line);
-				vector<string> values;
-				values.assign(istream_iterator<string>(linestream), istream_iterator<string>());
-
-				// Only lines with COL_NUM values on them are vertex lines.
-				if (values.size() == COL_NUM) {
-					// Reorder columns of the line.
-					outputFile << std::setprecision(6);
-					for (int i = 0; i < COL_NUM; i++) {
-						outputFile << values[col_order[i]] << " ";
-					};
-					outputFile << endl;
-				}
-				else {
-					outputFile << line << endl;
-				}
-
-			}
-
-			break;
-		}
-		else {
-			outputFile << line << endl;
-		}
-	}
-	inputFile.close();
-	outputFile.close();
-
-	return 0;
-}
